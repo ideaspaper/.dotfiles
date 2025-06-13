601c44c307c579f5dbe208a7341c852976ebbb60
anpan <agni.infinity@gmail.com>
2025-06-13 07:36:32 +0700

    refactor: Update Neovim plugin configurations
    Updated render-markdown.nvim repo to
    MeanderingProgrammer/render-markdown.nvim and added essential
    dependencies. Separated gitsigns.nvim into its own config block.
    Gitsigns keymaps now buffer-local via 'bufnr' in on_attach.

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -69,8 +69,9 @@ return {
 		end,
 	},
 	{
-		"toppairs/render-markdown.nvim",
+		"MeanderingProgrammer/render-markdown.nvim",
 		event = "VeryLazy",
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
 		config = function()
 			require("render-markdown").setup({})
 			vim.api.nvim_create_user_command("ToggleBufferRenderMarkdown", function()
@@ -88,49 +89,48 @@ return {
 		end,
 	},
 	{
-		"MeanderingProgrammer/render-markdown.nvim",
+		"lewis6991/gitsigns.nvim",
 		event = "VeryLazy",
-		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 		config = function()
 			require("gitsigns").setup({
-				on_attach = function()
-					local gs = require("gitsigns")
-					vim.keymap.set("n", "]c", function()
-						gs.next_hunk()
-					end, { desc = "next hunk" })
-					vim.keymap.set("n", "[c", function()
-						gs.prev_hunk()
-					end, { desc = "prev hunk" })
-					vim.keymap.set("n", "<leader>cs", gs.stage_hunk, { desc = "stage hunk" })
-					vim.keymap.set("n", "<leader>cu", gs.undo_stage_hunk, { desc = "undo stage hunk" })
-					vim.keymap.set("n", "<leader>cr", gs.reset_hunk, { desc = "reset hunk" })
-					vim.keymap.set("n", "<leader>cp", gs.preview_hunk, { desc = "preview hunk" })
-					vim.keymap.set("n", "<leader>cB", function()
-						gs.blame_line({ full = true })
-					end, { desc = "blame line" })
-					vim.keymap.set("n", "<leader>cD", gs.diffthis, { desc = "diff this" })
-					vim.keymap.set("n", "<leader>cS", ":Gitsigns stage_buffer<CR>", { desc = "stage buffer" })
-					vim.keymap.set("n", "<leader>cU", ":Gitsigns reset_buffer<CR>", { desc = "reset buffer" })
-					vim.keymap.set("v", "<leader>cs", function()
-						gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-					end, { desc = "stage selection" })
-					vim.keymap.set("v", "<leader>gr", function()
-						gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-					end, { desc = "reset selection" })
-					vim.keymap.set(
-						{ "o", "x" },
-						"ih",
-						":<C-U>Gitsigns select_hunk<CR>",
-						{ desc = "inner hunk text object" }
-					)
-					vim.keymap.set(
-						{ "o", "x" },
-						"ah",
-						":<C-U>Gitsigns select_hunk<CR>",
-						{ desc = "around hunk text object" }
-					)
-					vim.keymap.set("n", "<leader>cb", gs.toggle_current_line_blame, { desc = "toggle blame" })
-				end,
+                on_attach = function(bufnr)
+                local gs = require("gitsigns")
+                vim.keymap.set("n", "]c", function()
+                    gs.next_hunk()
+                end, { desc = "next hunk", buffer = bufnr })
+                vim.keymap.set("n", "[c", function()
+                    gs.prev_hunk()
+                end, { desc = "prev hunk", buffer = bufnr })
+                vim.keymap.set("n", "<leader>cs", gs.stage_hunk, { desc = "stage hunk", buffer = bufnr })
+                vim.keymap.set("n", "<leader>cu", gs.undo_stage_hunk, { desc = "undo stage hunk", buffer = bufnr })
+                vim.keymap.set("n", "<leader>cr", gs.reset_hunk, { desc = "reset hunk", buffer = bufnr })
+                vim.keymap.set("n", "<leader>cp", gs.preview_hunk, { desc = "preview hunk", buffer = bufnr })
+                vim.keymap.set("n", "<leader>cB", function()
+                    gs.blame_line({ full = true })
+                end, { desc = "blame line", buffer = bufnr })
+                vim.keymap.set("n", "<leader>cD", gs.diffthis, { desc = "diff this", buffer = bufnr })
+                vim.keymap.set("n", "<leader>cS", ":Gitsigns stage_buffer<CR>", { desc = "stage buffer", buffer = bufnr })
+                vim.keymap.set("n", "<leader>cU", ":Gitsigns reset_buffer<CR>", { desc = "reset buffer", buffer = bufnr })
+                vim.keymap.set("v", "<leader>cs", function()
+                    gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+                end, { desc = "stage selection", buffer = bufnr })
+                vim.keymap.set("v", "<leader>gr", function()
+                    gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+                end, { desc = "reset selection", buffer = bufnr })
+                vim.keymap.set(
+                    { "o", "x" },
+                    "ih",
+                    ":<C-U>Gitsigns select_hunk<CR>",
+                    { desc = "inner hunk text object", buffer = bufnr }
+                )
+                vim.keymap.set(
+                    { "o", "x" },
+                    "ah",
+                    ":<C-U>Gitsigns select_hunk<CR>",
+                    { desc = "around hunk text object", buffer = bufnr }
+                )
+                vim.keymap.set("n", "<leader>cb", gs.toggle_current_line_blame, { desc = "toggle blame", buffer = bufnr })
+            end,
 			})
 		end,
 	},
