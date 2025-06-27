82ce1c78dc21d2d3ae2afe4df67eefa7944dae68
anpan <agni.infinity@gmail.com>
2025-06-28 03:02:45 +0700

    feat: Add git validation and new gitroot function

--- zsh/.zsh/08_functions.zsh
+++ zsh/.zsh/08_functions.zsh
@@ -1,6 +1,20 @@
+_validate_git_installed() {
+  command -v git >/dev/null 2>&1 || {
+    echo "Git is not installed. Please install Git to use this command."
+    return 1
+  }
+}
+
+_validate_git_repo() {
+  if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+    echo "Not inside a Git repository." >&2
+    return 1
+  fi
+}
+
 mkcd() {
   if [ -z "$1" ]; then
-    echo "⚠️ Usage: mkcd <directory>"
+    echo "Usage: mkcd <directory>"
     return 1
   fi
 
@@ -9,17 +23,14 @@ mkcd() {
 
 mkproj() {
   if [ -z "$1" ]; then
-    echo "⚠️  Usage: mkproj <project-name>"
+    echo "Usage: mkproj <project-name>"
     return 1
   fi
 
-  if ! command -v git >/dev/null 2>&1; then
-    echo "❌ Git is not installed. Please install Git to use this command."
-    return 1
-  fi
+  _validate_git_installed || return 1
 
   if [ -d "$1" ]; then
-    echo "❌ Folder '$1' already exists. Aborting."
+    echo "Folder '$1' already exists. Aborting."
     return 1
   fi
 
@@ -33,5 +44,13 @@ mkproj() {
   git add README.md
   git commit -m "feat: Initial commit" >/dev/null 2>&1
 
-  echo "✅ Project '$folder_name' created and Git repository initialized."
+  echo "Project '$folder_name' created and Git repository initialized."
+}
+
+gitroot() {
+  _validate_git_installed || return 1
+  _validate_git_repo || return 1
+
+  cd "$(git rev-parse --show-toplevel)" || return 1
+  echo "Moved to Git root: $PWD"
 }
