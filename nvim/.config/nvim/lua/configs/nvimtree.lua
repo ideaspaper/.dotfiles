bd4b181b867a1f708f953924610a4ba8a6b8e159
Daniel Kristianto Haryono <daniel.haryono@shopee.com>
2025-07-04 16:57:45 +0700

    feat: Update nvimtree filters
    Update nvimtree filters to include git_ignored files.

--- nvim/.config/nvim/lua/configs/nvimtree.lua
+++ nvim/.config/nvim/lua/configs/nvimtree.lua
@@ -1,7 +1,10 @@
 dofile(vim.g.base46_cache .. "nvimtree")
 
 return {
-    filters = { dotfiles = false },
+    filters = {
+        dotfiles = false,
+        git_ignored = false,
+    },
     disable_netrw = true,
     hijack_cursor = true,
     sync_root_with_cwd = true,
